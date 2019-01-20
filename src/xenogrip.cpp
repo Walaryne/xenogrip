edd0e622187da51894151525c41ddc2d498e0a63
Walaryne <hazardoustheradioactive@gmail.com>
2019-01-20 02:10:34 -0500

    Finalized the address resolver

--- src/xenogrip.cpp
+++ src/xenogrip.cpp
@@ -4,10 +4,12 @@
 #include <errno.h>
 #include <error.h>
 #include <stdlib.h>
+#include "xenogrip.hpp"
 
 void pinfo(char buf[]) {
     printf("[xeno]INFO: %s\n", buf);
 }
+
 
 int test() {
     printf("kek it worked.\n");
@@ -21,14 +23,33 @@ int xeno(void) {
     int retval = mprotect((void *) 0x00400000, 10816, PROT_WRITE | PROT_EXEC);
     if(retval == 0) {
         pinfo((char *) "SUCCESSFULLY UNLOCKED MEMORY");
-        //unsigned char instr[] = "\x48\xb8\x10\x21\xff\xf7\xff\x7f\x00\x00\xff\xe0";
-        unsigned char instr[] = "\x48\xb8\xff\xff\xff\xff\xff\xff\x00\x00\xff\xe0";
-        //instr[7]
+        unsigned char tip[] = "\x48\xb8";
+        unsigned char mid[6];
+        unsigned char tail[] = "\x00\x00\xff\xe0";
+        char addrstr[14];
+        char hexbuf[2];
+        void *initialwp = (void *) 0x00402a00;
+        void *writeptr;
+        int counter = 5;
 
-        memmove((void *) 0x00402a00, instr, 12);
-        pinfo((char *) "SUCCESSFULLY WROTE NEW INSTRUCTIONS TO 0x00402a00");
-        printf("[xeno]ERRORNO: %d\n", errno);
-        printf("[xeno]TESTFUNC: %p\n", test);
+        sprintf(addrstr, "%p", test);
+        for(int i = 2; i != 14; i += 2) {
+            for(int j = 0; j != 2; j++) {
+                hexbuf[j] = addrstr[i + j];
+                if(j == 1){
+                    mid[counter] = hex2int(hexbuf);
+                    counter--;
+                }
+            }
+        }
+
+        writeptr = mempcpy(initialwp, tip, 2);
+        writeptr = mempcpy(writeptr, mid, 6);
+        writeptr = mempcpy(writeptr, tail, 4);
+        printf("[xeno]INFO: Overwrote instructions at %p\n", initialwp);
+        //printf("[xeno]ERRORNO: %d\n", errno);
+        //printf("[xeno]TESTFUNC: %p\n", test);
+        //printf("[xeno]HEX2INT: %d\n", hex2int((char *)"2d"));
     } else {
         pinfo((char *) "ERROR UNLOCKING MEMORY");
         printf("[xeno]ERRORNO: %d\n", errno);
---
+++ src/xenogrip.hpp
@@ -0,0 +1,6 @@
+#ifndef XENOGRIP_HPP_
+#define XENOGRIP_HPP_
+
+int hex2int(char buf[2]);
+
+#endif /* XENOGRIP_HPP_ */
