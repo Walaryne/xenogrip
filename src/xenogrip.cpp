f74cca4e8003b46da7c7e28a874a36bd618fb91f
Walaryne <hazardoustheradioactive@gmail.com>
2019-01-19 22:11:23 -0500

    Initial commit

---
+++ src/xenogrip.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <errno.h>
+#include <error.h>
+#include <stdlib.h>
+
+void pinfo(char buf[]) {
+    printf("[xeno]INFO: %s\n", buf);
+}
+
+int test() {
+    printf("kek it worked.\n");
+    exit(0);
+    return 0;
+}
+
+int xeno(void) {
+    __asm__(".section .init \n call _Z4xenov \n .section .text\n");
+    pinfo((char *) "Xenogrip loaded!");
+    int retval = mprotect((void *) 0x00400000, 10816, PROT_WRITE | PROT_EXEC);
+    if(retval == 0) {
+        pinfo((char *) "SUCCESSFULLY UNLOCKED MEMORY");
+        //unsigned char instr[] = "\x48\xb8\x10\x21\xff\xf7\xff\x7f\x00\x00\xff\xe0";
+        unsigned char instr[] = "\x48\xb8\xff\xff\xff\xff\xff\xff\x00\x00\xff\xe0";
+        //instr[7]
+        int pos = 2;
+        unsigned char addr[12];
+        char pstr[15];
+        snprintf(pstr, 15, "%p", test);
+        for(int i = 7; i != 1; i--) {
+            for(int i = 0; i != 2; i++) {
+                pstr[]
+                pos += 2;
+            }
+        }
+        memmove((void *) 0x00402a00, instr, 12);
+        pinfo((char *) "SUCCESSFULLY WROTE NEW INSTRUCTIONS TO 0x00402a00");
+        printf("[xeno]ERRORNO: %d\n", errno);
+        printf("[xeno]TESTFUNC: %p\n", test);
+        printf("[xeno]PSTR: %s\n", pstr);
+    } else {
+        pinfo((char *) "ERROR UNLOCKING MEMORY");
+        printf("[xeno]ERRORNO: %d\n", errno);
+    }
+    return 0;
+}
